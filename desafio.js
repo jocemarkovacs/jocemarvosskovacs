345df409e5e4534238b7fe59e212c0e306362e52
Jocemar <jocemarvosskovacs@gmail.com>
2022-06-23 17:06:11 -0300

    Entrega do desafio de Lógica de Programação em Python e JavaScript

---
+++ desafio.js
@@ -0,0 +1,1 @@
+for(let i =0; i<101; i++){if(i%5 === 0 && i%3 === 0){console.log('Universo')} else if (i%3 === 0){console.log('Uni')} else if(i%5 === 0){console.log('verso')} else {console.log(i)}}
---
+++ desafio.py
@@ -0,0 +1,5 @@
+for i in range(1, 101):
+    if (i%3) == 0 and (i%5) == 0: print('Universo')
+    elif (i%3) == 0: print('Uni')
+    elif (i%5) == 0: print('verso')
+    else: print(i)
